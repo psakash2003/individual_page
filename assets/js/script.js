d45477190f0d421f1f3172001b7f3606788fd8a0
Akash PS <100912220+psakash2003@users.noreply.github.com>
2023-05-15 07:45:51 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -241,7 +241,7 @@ srtop.reveal('.education .box', { interval: 200 });
 srtop.reveal('.education .container .bar', { delay: 400 });
 
 /* SCROLL PROJECTS */
-srtop.reveal('.work .container', { interval: 200 });
+srtop.reveal('.work .box', { interval: 200 });
 srtop.reveal('.work .container .bar', { delay: 400 });
 
 /* SCROLL EXPERIENCE */
